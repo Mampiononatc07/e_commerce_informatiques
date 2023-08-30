4fcbf38e9212bc194a22c696417a756f6339d20d
Mampionona <hei.mampionona@gmail.com>
2023-08-30 11:46:11 +0300

    Feat: add e_commerce_informatiques Application

---
+++ e_commerce_informatiques/src/main/java/com/example/e_commerce_informatiques/eCommerceApplication.java
@@ -0,0 +1,11 @@
+package com.example.e_commerce_informatiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class eCommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(eCommerceApplication.class, args);
+    }
+}
